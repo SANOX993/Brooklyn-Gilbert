a90165defa89f6a27d6c1cd88f0552eb6e4f4770
SANOX993 <danilyatsiuk@gmail.com>
2022-11-07 22:50:14 +0200

    commit

---
+++ scripts/jobschanger.js
@@ -0,0 +1,27 @@
+/*let changerCard = document.querySelectorAll('.changer-card');
+
+changerCard.addEventListener('click', () => {
+    changerCard.classList.toggle('active');
+})*/
+
+/*$('.changer-card').click(function(){
+    $('.changer-card').toggleClass('active');
+  }); */
+
+let changerCard1 = document.querySelector('.changer-card1');
+
+changerCard1.addEventListener('click', () => {
+    changerCard1.classList.toggle('active');
+})
+
+let changerCard2 = document.querySelector('.changer-card2');
+
+changerCard2.addEventListener('click', () => {
+    changerCard2.classList.toggle('active');
+})
+
+let changerCard3 = document.querySelector('.changer-card3');
+
+changerCard3.addEventListener('click', () => {
+    changerCard3.classList.toggle('active');
+})
